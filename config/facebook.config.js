f7f45be8e70a0631004f35747c7a664bb7930055
Tolojanahary RABEFALY <tolojanahary.nexthope@gmail.com>
2025-09-27 21:08:57 +0300

    Update Facebook Graph API version to v23.0 in configuration

--- config/facebook.config.js
+++ config/facebook.config.js
@@ -12,7 +12,7 @@ export const FACEBOOK_WEBHOOK_VERIFY_TOKEN = process.env.FACEBOOK_WEBHOOK_VERIFY
 export const FACEBOOK_SCOPE = ['email', 'public_profile', 'pages_messaging'];
 export const FACEBOOK_PAGE_ACCESS_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
 export const FACEBOOK_PAGE_DEEP_LINK_URL = `https://m.me/${FACEBOOK_PAGE_ID}?ref=link_account`;
-export const META_GRAPH_API_VERSION = 'v19.0';
+export const META_GRAPH_API_VERSION = 'v23.0';
 export const META_GRAPH_API_URL = `https://graph.facebook.com/${META_GRAPH_API_VERSION}`;
 export const FACEBOOK_ACCOUNT_LINKING_TOKEN_SECRET = process.env.FACEBOOK_ACCOUNT_LINKING_TOKEN_SECRET;
 export const FACEBOOK_ACCOUNT_LINKING_TOKEN_EXPIRATION = 60 * 2;
